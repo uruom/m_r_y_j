f5b8dbc98b74bf5c0fdb58f7ba50b297622b82f5
HenryZ5734 <200110614@stu.hit.edu.cn>
2022-05-31 17:19:49 +0800

    重构代码，将导航界面设置为检测界面

--- app/src/main/java/com/example/m_r_y_j/MainActivity.java
+++ app/src/main/java/com/example/m_r_y_j/MainActivity.java
@@ -2,21 +2,16 @@ package com.example.m_r_y_j;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActivityManager;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView m_title;
-    private Button mBtn_start,mBtn_instruction,mBtn_control,mBtn_start_test;
+    private Button mBtn_start,mBtn_instruction,mBtn_settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,22 +20,20 @@ public class MainActivity extends AppCompatActivity {
 
         // 链接组件
         m_title = findViewById(R.id.tv_title);
-        mBtn_instruction = findViewById(R.id.btn_instruction);
-        mBtn_control = findViewById(R.id.btn_control);
         mBtn_start = findViewById(R.id.btn_start);
-        mBtn_start_test = findViewById(R.id.btn_start_test);
+        mBtn_instruction = findViewById(R.id.btn_instruction);
+        mBtn_settings = findViewById(R.id.btn_control);
 
         // 设置字体
         Typeface typeface = Typeface.createFromAsset(getAssets(),"font/ziti_1.ttf");
         m_title.setTypeface(typeface);
         mBtn_start.setTypeface(typeface);
         mBtn_instruction.setTypeface(typeface);
-        mBtn_control.setTypeface(typeface);
-        mBtn_start_test.setTypeface(typeface);
+        mBtn_settings.setTypeface(typeface);
 
         // 跳转到运行界面
         mBtn_start.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this,StartActivity.class);
+            Intent intent = new Intent(MainActivity.this,DetectActivity.class);
             startActivity(intent);
         });
 
@@ -51,14 +44,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //跳转到设置界面
-        mBtn_control.setOnClickListener(view -> {
+        mBtn_settings.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this,ControlActivity.class);
-            startActivity(intent);
-        });
-
-        // 跳转到运行界面
-        mBtn_start_test.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this,DetectActivity.class);
             startActivity(intent);
         });
     }
